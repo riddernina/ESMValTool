71ce415a7046a441bff52aa9c4a96995ca461371
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-01-24 10:34:25 +0000

    multimodel stats - first good draft that accounts for all requirements from issue 168

--- esmvaltool/preprocessor/_multimodel.py
+++ esmvaltool/preprocessor/_multimodel.py
@@ -13,9 +13,31 @@ from ._io import save_cubes
 logger = logging.getLogger(__name__)
 
 
-def _put_in_cube(stats, stats_name, ncfiles, fname):
-    """quick cube building"""
-    stats_cube = iris.cube.Cube(stats, long_name=stats_name)
+def _compute_stats(cubes, statname):
+    """compute any multimodel stats"""
+    if statname == 'means':
+        statistic = np.ma.mean([s.data for s in cubes], axis=0)
+    elif statname == 'medians':
+        statistic = np.ma.median([s.data for s in cubes], axis=0)
+    return statistic
+
+
+def _put_in_cube(cubes, stats_name, ncfiles, fname):
+    """quick cube building and saving"""
+    # grab coordinates from any cube
+    times = cubes[0].coord('time')
+    lats = cubes[0].coord('latitude')
+    lons = cubes[0].coord('longitude')
+    cspec = [(times, 0), (lats, 1), (lons, 2)]
+    # compute stats and put in cube
+    dspec = _compute_stats(cubes, stats_name)
+    fixed_dspec = np.ma.fix_invalid(dspec, copy=False, fill_value=1e+20)
+    stats_cube = iris.cube.Cube(fixed_dspec,
+                                dim_coords_and_dims=cspec,
+                                long_name=stats_name)
+    coord_names = [coord.name() for coord in cubes[0].coords()]
+    if 'air_pressure' in coord_names:
+        stats_cube.add_aux_coord(cubes[0].coord('air_pressure'))
     stats_cube.attributes['_filename'] = fname
     stats_cube.attributes['NCfiles'] = str(ncfiles)
     return stats_cube
@@ -64,10 +86,6 @@ def multi_model_mean(cubes, span, filename, exclude):
         logger.info("check models: will not compute statistics.")
         return cubes
     else:
-        # empty lists to hold data
-        tmeans = []
-        data_size = []
-
         # add file name info
         file_names = [
             os.path.basename(cube.attributes.get('_filename'))
@@ -76,6 +94,7 @@ def multi_model_mean(cubes, span, filename, exclude):
 
         # look at overlap type
         if span == 'overlap':
+            tmeans = []
             tx1, tx2 = _get_overlap(cubes)
             for cube in selection:
                 logger.debug("Using common time overlap between "
@@ -85,36 +104,60 @@ def multi_model_mean(cubes, span, filename, exclude):
                 with iris.FUTURE.context(cell_datetime_objects=True):
                     cube = _slice_cube(cube, tx1, tx2)
 
-                # store nr time points
-                data_size.append(len(list(cube.coord('time').points)))
-
-                # compute stats and append to lists
-                tmeans.append(cube.collapsed('time', iris.analysis.MEAN))
+                # record data
+                tmeans.append(cube)
 
         elif span == 'full':
-            for cube in selection:
-                logger.debug("Using full time spans "
-                             "to compute statistics.")
+            logger.debug("Using full time spans "
+                         "to compute statistics.")
+            tmeans = []
+            # make a copy
+            cpycubes = selection[:]
+            # lay down time points
+            tpts = [c.coord('time').points for c in cpycubes]
+            ft = []
+            for sublist in tpts:
+                for item in sublist:
+                    ft.append(item)
+            tx = list(set(ft))
+            tx.sort()
 
-                # store nr time points
-                data_size.append(len(list(cube.coord('time').points)))
-
-                # compute stats and append to lists
-                tmeans.append(cube.collapsed('time', iris.analysis.MEAN))
+            # loop through cubes and apply masks
+            for cube in cpycubes:
+                fine_shape = (len(tx),
+                              cube.data.shape[1],
+                              cube.data.shape[2])
+                # find indices of present time points
+                oidx = [tx.index(s) for s in cube.coord('time').points]
+                # reshape data to include all possible times
+                ndat = np.ma.resize(cube.data, fine_shape)
+                # build the time mask
+                c = np.ones(fine_shape, bool)
+                for ti in oidx:
+                    c[ti] = False
+                ndat.mask |= c
+                # build the new coords
+                time_c = iris.coords.DimCoord(
+                    tx, standard_name='time', units='days since 1950-1-1')
+                lat_c = cube.coord('latitude')
+                lon_c = cube.coord('longitude')
+                cspec = [(time_c, 0), (lat_c, 1), (lon_c, 2)]
+                ncube = iris.cube.Cube(ndat, dim_coords_and_dims=cspec)
+                coord_names = [coord.name() for coord in cube.coords()]
+                if 'air_pressure' in coord_names:
+                    ncube.add_aux_coord(cube.coord('air_pressure'))
+                ncube.attributes = cube.attributes
+                tmeans.append(ncube)
 
         else:
             logger.debug("No type of time overlap specified "
                          "- will not compute cubes statistics")
             return cubes
 
-    c_mean = _put_in_cube(np.mean([s.data for s in tmeans], axis=0),
-                          'means', file_names, filename)
+    c_mean = _put_in_cube(tmeans, 'means', file_names, filename)
 
-    c_med = _put_in_cube(np.median([s.data for s in tmeans], axis=0),
-                         'medians', file_names, filename)
+    c_med = _put_in_cube(tmeans, 'medians', file_names, filename)
 
-    c_dat = _put_in_cube(data_size, 'data_size', file_names, filename)
-
-    save_cubes([c_mean, c_med, c_dat])
+    save_cubes([c_mean, c_med])
 
     return cubes
