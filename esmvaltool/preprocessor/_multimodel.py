ccfa83db30165def2b0077308e7ea2de235e77b5
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-01-26 16:37:33 +0000

    now it works for an arbitrary number of plevs; functionality for different time axis units

--- esmvaltool/preprocessor/_multimodel.py
+++ esmvaltool/preprocessor/_multimodel.py
@@ -1,4 +1,9 @@
-"""Functions for multi-model operations"""
+"""Functions for multi-model operations
+   supports a multitude of multimodel statistics
+   computations; the only requisite is the ingested
+   cubes have (TIME-LAT-LON) or (TIME-PLEV-LAT-LON)
+   dimensions; and obviously consistent units.
+"""
 import logging
 from functools import reduce
 
@@ -28,7 +33,11 @@ def _put_in_cube(cubes, stats_name, ncfiles, fname):
     times = cubes[0].coord('time')
     lats = cubes[0].coord('latitude')
     lons = cubes[0].coord('longitude')
-    cspec = [(times, 0), (lats, 1), (lons, 2)]
+    if len(cubes[0].shape) == 3:
+        cspec = [(times, 0), (lats, 1), (lons, 2)]
+    elif len(cubes[0].shape) == 4:
+        plev = cubes[0].coord('air_pressure')
+        cspec = [(times, 0), (plev, 1), (lats, 2), (lons, 3)]
     # compute stats and put in cube
     dspec = _compute_stats(cubes, stats_name)
     fixed_dspec = np.ma.fix_invalid(dspec, copy=False, fill_value=1e+20)
@@ -37,25 +46,31 @@ def _put_in_cube(cubes, stats_name, ncfiles, fname):
                                 long_name=stats_name)
     coord_names = [coord.name() for coord in cubes[0].coords()]
     if 'air_pressure' in coord_names:
-        stats_cube.add_aux_coord(cubes[0].coord('air_pressure'))
+        if len(cubes[0].shape) == 3:
+            stats_cube.add_aux_coord(cubes[0].coord('air_pressure'))
     stats_cube.attributes['_filename'] = fname
     stats_cube.attributes['NCfiles'] = str(ncfiles)
     return stats_cube
 
 
-def _sdat(srl_no):
+def _sdat(srl_no, unit_type):
     """convert to a datatime point"""
-    new_date = dd(1950, 1, 1, 0) + td(srl_no)
+    if unit_type == 'day since 1950-01-01 00:00:00.0000000':
+        new_date = dd(1950, 1, 1, 0) + td(srl_no)
+    elif unit_type == 'day since 1850-01-01 00:00:00.0000000':
+        new_date = dd(1850, 1, 1, 0) + td(srl_no)
+    # add more supported units here
     return new_date
 
 
 def _get_overlap(cubes):
     """Get discrete time overlaps."""
+    utype = str(cubes[0].coord('time').units)
     all_times = [cube.coord('time').points for cube in cubes]
     bounds = [range(int(b[0]), int(b[-1]) + 1) for b in all_times]
     time_pts = reduce(np.intersect1d, (i for i in bounds))
     if len(time_pts) > 1:
-        return _sdat(time_pts[0]), _sdat(time_pts[-1])
+        return _sdat(time_pts[0], utype), _sdat(time_pts[-1], utype)
 
 
 def _slice_cube(cube, min_t, max_t):
@@ -111,41 +126,48 @@ def multi_model_mean(cubes, span, filename, exclude):
             logger.debug("Using full time spans "
                          "to compute statistics.")
             tmeans = []
-            # make a copy
-            cpycubes = selection[:]
             # lay down time points
-            tpts = [c.coord('time').points for c in cpycubes]
-            ft = []
-            for sublist in tpts:
-                for item in sublist:
-                    ft.append(item)
-            tx = list(set(ft))
+            tpts = [c.coord('time').points for c in selection]
+            tx = list(set().union(*tpts))
             tx.sort()
 
             # loop through cubes and apply masks
-            for cube in cpycubes:
-                fine_shape = (len(tx),
-                              cube.data.shape[1],
-                              cube.data.shape[2])
+            for cube in selection:
+                # construct new shape
+                fine_shape = tuple([len(tx)] + list(cube.data.shape[1:]))
                 # find indices of present time points
                 oidx = [tx.index(s) for s in cube.coord('time').points]
                 # reshape data to include all possible times
                 ndat = np.ma.resize(cube.data, fine_shape)
+
                 # build the time mask
                 c = np.ones(fine_shape, bool)
                 for ti in oidx:
                     c[ti] = False
                 ndat.mask |= c
+
                 # build the new coords
+                # preserve units
                 time_c = iris.coords.DimCoord(
-                    tx, standard_name='time', units='days since 1950-1-1')
+                    tx, standard_name='time',
+                    units=cube.coord('time').units)
                 lat_c = cube.coord('latitude')
                 lon_c = cube.coord('longitude')
-                cspec = [(time_c, 0), (lat_c, 1), (lon_c, 2)]
+
+                # time-lat-lon
+                if len(fine_shape) == 3:
+                    cspec = [(time_c, 0), (lat_c, 1), (lon_c, 2)]
+                # time-plev-lat-lon
+                elif len(fine_shape) == 4:
+                    pl_c = cube.coord('air_pressure')
+                    cspec = [(time_c, 0), (pl_c, 1), (lat_c, 2), (lon_c, 3)]
+
+                # build cube
                 ncube = iris.cube.Cube(ndat, dim_coords_and_dims=cspec)
                 coord_names = [coord.name() for coord in cube.coords()]
                 if 'air_pressure' in coord_names:
-                    ncube.add_aux_coord(cube.coord('air_pressure'))
+                    if len(fine_shape) == 3:
+                        ncube.add_aux_coord(cube.coord('air_pressure'))
                 ncube.attributes = cube.attributes
                 tmeans.append(ncube)
 
